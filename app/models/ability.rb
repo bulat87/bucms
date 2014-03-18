8fc07fbc030a77eebf99d8b876ec6f5f1f5b51d7
bu <bulat87@gmail.com>
2014-03-19 03:40:26 +0400

    fix comment delete

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -4,11 +4,14 @@ class Ability
   def initialize(user)
     user ||= User.new
 
-    if user.role == "reader"
+    if user.role == "admin"
       can :manage, :all
-    else
+    elsif user.role == "reader"
       can :read, :all
       can :create, Comment
+      can :delete, Comment do |comment|
+        comment.try(:user) == user
+      end
     end
   end
 end
